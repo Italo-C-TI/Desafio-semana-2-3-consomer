52206755f122a76061f8bc2c1e2c2f7ed4fdae1a
Italo Costa <costa.italo.ti@gmail.com>
2022-05-07 15:20:02 -0300

    Adiconando funcionalidade de consomer kafka

--- src/main/java/br/com/sqs_consomer/App.java
+++ src/main/java/br/com/sqs_consomer/App.java
@@ -1,15 +1,26 @@
 package br.com.sqs_consomer;
 
 import br.com.sqs_consomer.services.SQSService;
+import br.com.sqs_consomer.services.KafkaService;
+import java.util.concurrent.ExecutionException;
 
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException
-    {
-        System.out.println("Lendo mensagens ...");
-        while(true){
-            SQSService.messageReader();
-        }
+     // para sqsSystem.getEnv(
+    // public static v)oid main( String[] args ) throws InterruptedException
+    // {
+    //     System.out.println("Lendo mensagens ...");
+    //     while(true){
+    //         SQSService.messageReader();
+    //     }
+    // }
+
+    // para kafka
+    public static void main(String[] args) throws InterruptedException, ExecutionException{
+         System.out.println("Lendo mensagens ...");
+         while(true){
+             KafkaService.readMessage(System.getenv("KAFKA_GROUP_ID_READER"));
+         }
     }
 }
 
--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export CLASSPATH=target/sqs_consomer-1.0-SNAPSHOT.jar
 export className=App
+export KAFKA_HOST="localhost:9092"
+export KAFKA_TOPIC="TOPICO_1"
+export KAFKA_GROUP_ID_READER="consome_1"
 echo "## Running $className..."
 shift
 mvn exec:java -Dexec.mainClass="br.com.sqs_consomer.$className"
